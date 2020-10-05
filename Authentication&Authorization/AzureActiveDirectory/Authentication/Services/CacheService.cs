be41959ed80eb4ed250033d92dfb984f2f087704
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2020-10-05 14:17:32 +0200

    add cache service

---
+++ Authentication&Authorization/AzureActiveDirectory/Authentication/Services/CacheService.cs
@@ -0,0 +1,32 @@
+﻿using Authentication.Services.Contract;
+using Authentication.Settings;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Services
+{
+    public class CacheService<T> : ICacheService<T> where T : class, new ()
+    {
+        private readonly IMemoryCache _memoryCache;
+        public CacheService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+
+        public void SetValue(object key, T value)
+        {
+            _memoryCache.Set(key, value);
+        }
+
+        public T TryGetValue(object key)
+        {
+            if (_memoryCache.TryGetValue<T>(key, out T value)) return value;
+
+            return null;
+        }
+    }
+}
---
+++ Authentication&Authorization/AzureActiveDirectory/Authentication/Services/Contract/ICacheService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Services.Contract
+{
+    /// <summary>
+    /// Cache service
+    /// </summary>
+    public interface ICacheService<T> where T: class, new()
+    {
+        /// <summary>
+        /// Try to get value from cache.
+        /// </summary>
+        /// <param name="key">Cache key.</param>
+        /// <returns>Value which referancial to key.</returns>
+        T TryGetValue(object key);
+
+        /// <summary>
+        /// Set value to cache.
+        /// </summary>
+        /// <param name="key">Cache key.</param>
+        /// <param name="value">Cache value.</param>
+        void SetValue(object key, T value);
+    }
+}
