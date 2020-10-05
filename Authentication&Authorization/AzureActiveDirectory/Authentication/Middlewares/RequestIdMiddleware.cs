04d8ef9a84160902485671b29e09a384d1cc49e4
Nikita Mykhailenko <nickitamykhailneko@gmail.com>
2020-10-05 11:36:30 +0200

    add requestId middleware

---
+++ Authentication&Authorization/AzureActiveDirectory/Authentication/Middlewares/RequestIdMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Middlewares
+{
+    public class RequestIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var requetId = Guid.NewGuid().ToString();
+
+            // add custom data of request for private use
+            if(!context.Request.Headers.ContainsKey("request-id"))
+                context.Request.Headers.Add("request-id", requetId);
+
+            // add custom information for public (Web API response).
+            if (!context.Response.Headers.ContainsKey("request-id"))
+                context.Response.Headers.Add("request-id", requetId);
+
+            await _next(context);
+
+        }
+    }
+}
+
